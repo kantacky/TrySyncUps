12bfce4a81be02d3e3dbab2fb5341cb1eed04a5a
Kanta Oikawa <k.oikawa@kantacky.com>
2024-03-24 12:51:44 +0900

    wip

--- starting-point/TrySyncUpsTests/SyncUpsListTests.swift
+++ starting-point/TrySyncUpsTests/SyncUpsListTests.swift
@@ -37,6 +37,50 @@ final class SyncUpsListTests: XCTestCase {
   }
 
   @MainActor
+  func testDeletion() async {
+    let syncUp = SyncUp.mock
+    let store = TestStore(initialState: SyncUpsListFeature.State(syncUps: [syncUp])) {
+      SyncUpsListFeature()
+    }
+
+    await store.send(.syncUpTapped(id: syncUp.id)) {
+      $0.destination = .syncUpDetail(SyncUpDetailFeature.State(syncUp: Shared(syncUp)))
+    }
+    await store.send(\.destination.syncUpDetail.deleteButtonTapped) {
+      $0.destination?.syncUpDetail?.alert = AlertState {
+        TextState("Are you sure you want to delete this sync up?")
+      } actions: {
+        ButtonState(role: .destructive, action: .confirmDeletion) {
+          TextState("Delete")
+        }
+      }
+    }
+    await store.send(\.destination.syncUpDetail.alert.confirmDeletion) {
+      $0.destination?.syncUpDetail?.alert = nil
+    }
+    await store.receive(\.destination.dismiss) {
+      $0.destination = nil
+      $0.syncUps = []
+    }
+  }
+
+  @MainActor
+  func testDeletion_NonExhaustive() async {
+    let syncUp = SyncUp.mock
+    let store = TestStore(initialState: SyncUpsListFeature.State(syncUps: [syncUp])) {
+      SyncUpsListFeature()
+    }
+    store.exhaustivity = .off
+
+    await store.send(.syncUpTapped(id: syncUp.id))
+    await store.send(\.destination.syncUpDetail.deleteButtonTapped)
+    await store.send(\.destination.syncUpDetail.alert.confirmDeletion)
+    await store.receive(\.destination.dismiss) {
+      $0.syncUps = []
+    }
+  }
+
+  @MainActor
   func testModel() {
     let model = SyncUpsListModel(syncUps: [.mock, .productMock])
     model.onDelete([1])
